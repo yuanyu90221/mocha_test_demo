b68584600973def3c1208232cedb2cbca37cf438
yuanyu90221 <yuanyu90221@gmail.com>
2019-01-06 00:34:22 +0800

    add test for promise

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
-
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 let demo = require('../lib/demo');
 
 describe('demo', () =>{
@@ -30,5 +31,21 @@ describe('demo', () =>{
         done(ex);
       });
     });
+
+    it('should test with a promise return', () => {
+      demo.addPromise(1,2).then((result)=>{
+        expect(result).to.equal(3)
+      });
+    });
+
+    it('should test promise with async await', async ()=>{
+      let result = await demo.addPromise(1,2);
+
+      expect(result).to.equal(3);
+    });
+
+    it('should test promise with chai as promise', async ()=>{
+      await expect(demo.addPromise(1,2)).to.eventually.equal(3);
+    })
   });
 });

ac1dd46f6796df44552ed952235b518fd5244a07
yuanyu90221 <yuanyu90221@gmail.com>
2019-01-06 01:08:32 +0800

    add stub with sinon api

--- test/demo.test.js
+++ test/demo.test.js
@@ -2,7 +2,12 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
-let demo = require('../lib/demo');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+const rewire = require('rewire');
+let demo = rewire('../lib/demo');
 
 describe('demo', () =>{
   context('add', () => {
@@ -47,5 +52,37 @@ describe('demo', () =>{
     it('should test promise with chai as promise', async ()=>{
       await expect(demo.addPromise(1,2)).to.eventually.equal(3);
     })
+  });
+
+  context('test double', ()=> {
+    it('should spy on log', ()=>{
+      let spy = sinon.spy(console, 'log');
+      demo.foo();
+      expect(spy.calledOnce).to.be.true;
+      expect(spy).to.have.been.calledOnce;
+      spy.restore();
+    });
+
+    it('should stub console.warn', ()=> {
+      let stub = sinon.stub(console, 'warn').callsFake(()=>{ console.log(`message from stub`)});
+      demo.foo();
+      expect(stub).to.have.been.calledOnce;
+      expect(stub).to.have.been.calledWith('console.warn was called');
+      stub.restore();
+    });
+  });
+
+  context('stub private function', () => {
+    it('should stub create file', async () => {
+      let createStub = sinon.stub(demo, 'createFile').resolves('create_stub');
+      let callStub = sinon.stub().resolves('calldb_stub');
+
+      demo.__set__('callDB', callStub);
+      let result = await demo.bar('test.txt');
+      expect(result).to.equal('calldb_stub');
+      expect(createStub).to.have.been.calledOnce;
+      expect(createStub).to.have.been.calledWith('test.txt')
+      expect(callStub).to.have.been.calledOnce;
+    });
   });
 });

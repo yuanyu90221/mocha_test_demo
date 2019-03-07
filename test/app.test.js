6ce4f7a53f21390d1266ee2282f65bd2a0e8ce13
yuanyu90221 <yuanyu90221@gmail.com>
2019-03-08 00:31:35 +0800

    add app.test.js

---
+++ test/app.test.js
@@ -0,0 +1,67 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+const rewire = require('rewire');
+const request = require('supertest');
+
+let app = rewire('../lib/app');
+const users = require('../lib/users');
+const auth = require('../lib/auth');
+const sandbox = sinon.sandbox.create();
+
+describe('app', ()=>{
+  afterEach(()=>{
+    app = rewire('../lib/app');
+    sandbox.restore();
+  });
+
+  context('GET /', () => {
+    it('should get /', (done)=> {
+      request(app).get('/')
+        .expect(200)
+        .end((err, response)=>{
+          expect(response.body).to.have.property('name').to.equal('Foo Fooing Bar');
+          done(err);
+        });
+    });
+  });
+
+  context('POST /user', ()=>{
+    let createStub, errorStub;
+    
+    it('should call user.create', (done)=>{
+      createStub = sandbox.stub(users, 'create').resolves({name: 'foo'});
+      request(app).post('/user')
+        .send({name: 'fake'})
+        .expect(200)
+        .end((err, response)=>{
+          expect(createStub).to.have.been.calledOnce;
+          expect(response.body).to.have.property('name').to.equal('foo');
+          done(err);
+        })
+    });
+
+    it('should call handleError on error', (done)=>{
+      createStub = sandbox.stub(users, 'create').rejects(new Error('fake_error'));
+
+      errorStub = sandbox.stub().callsFake((res, error)=>{
+        return res.status(400).json({error: 'fake'});
+      });
+
+      app.__set__('handleError', errorStub);
+      request(app).post('/user')
+        .send({name: 'fake'})
+        .expect(400)
+        .end((err, response)=>{
+          expect(createStub).to.have.been.calledOnce;
+          expect(errorStub).to.have.been.calledOnce;
+          expect(response.body).to.have.property('error').to.equal('fake');
+          done(err);
+        });
+    });
+  });
+});
